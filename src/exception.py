4304770b319d09495fa7635ce107a5bc5fc67834
Nzx03 <nzx786@gmail.com>
2025-08-26 17:57:22 +0530

    logging and exceptions

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys 
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    # this for execution info which will give 3 info  ( in which first 2 info is not of my use)
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+     file_name, exc_tb.tb_lineno,str(error))
+
+    return error_message   #error message
+
+class CustonException(Exception):
+    def __init__(self, error_message,error_detail:sys):  #constructor
+        super().__init__(error_message)  #for inheriting the __init__ function (from exception)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message #going to print excpetion
+    
+
---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging #any excetution happening is get stored in log file
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"  #this willbe txt file
+
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)   #for getting working directory
+os.makedirs(logs_path,exist_ok=True)
+
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(filename=LOG_FILE_PATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+                    level=logging.INFO,
+                    
+                    
+                    
+                    
+                    )
+
+
